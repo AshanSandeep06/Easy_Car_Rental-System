33d938c3258dc4eafa5690bd9eafe937507566ef
AshanSandeep06 <ashansandeep06@gmail.com>
2023-02-13 10:34:16 +0530

    Added advisor class to handle the exceptions

---
+++ Back-End/src/main/java/lk/easy/carRental/advisor/AppWideExceptionHandler.java
@@ -0,0 +1,17 @@
+package lk.easy.carRental.advisor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@CrossOrigin
+public class AppWideExceptionHandler {
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler({RuntimeException.class})
+    public String handleMyExceptions(RuntimeException e){
+        System.out.println(e.getMessage());
+        return e.getMessage();
+    }
+}
