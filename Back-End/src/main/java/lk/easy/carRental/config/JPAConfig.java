93837c6962dd155954061ac396277976da0f4c08
AshanSandeep06 <ashansandeep06@gmail.com>
2023-02-13 10:52:03 +0530

    Fully Refactored JPAConfig configuration class

--- Back-End/src/main/java/lk/easy/carRental/config/JPAConfig.java
+++ Back-End/src/main/java/lk/easy/carRental/config/JPAConfig.java
@@ -9,6 +9,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -27,6 +28,7 @@ public class JPAConfig {
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource ds, JpaVendorAdapter va){
         LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+        factoryBean.setPackagesToScan(environment.getRequiredProperty("config.entity"));
         factoryBean.setDataSource(ds);
         factoryBean.setJpaVendorAdapter(va);
         return factoryBean;
@@ -35,12 +37,20 @@ public class JPAConfig {
     @Bean
     public DataSource dataSource(){
         DriverManagerDataSource bds = new DriverManagerDataSource();
+        bds.setDriverClassName(environment.getRequiredProperty("config.driver"));
+        bds.setUrl(environment.getRequiredProperty("config.url"));
+        bds.setUsername(environment.getRequiredProperty("config.username"));
+        bds.setPassword(environment.getRequiredProperty("config.password"));
         return bds;
     }
 
     @Bean
     public JpaVendorAdapter jpaVendorAdapter(){
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setDatabasePlatform(environment.getRequiredProperty("config.dialect"));
+        vendorAdapter.setDatabase(Database.MYSQL);
+        vendorAdapter.setGenerateDdl(true);
+        vendorAdapter.setShowSql(true);
         return vendorAdapter;
     }
 
