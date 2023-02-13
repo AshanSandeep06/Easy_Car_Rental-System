ec9e79ae0f981e582e5fb1afc751d34044eb49e3
AshanSandeep06 <ashansandeep06@gmail.com>
2023-02-13 10:55:14 +0530

    Added WebAppInitializer class

---
+++ Back-End/src/main/java/lk/easy/carRental/WebAppInitializer.java
@@ -0,0 +1,23 @@
+package lk.easy.carRental;
+
+import lk.easy.carRental.config.WebAppConfig;
+import lk.easy.carRental.config.WebRootConfig;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{WebRootConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
