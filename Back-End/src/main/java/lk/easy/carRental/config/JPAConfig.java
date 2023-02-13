29fff2df3b9cd59ebc3b3858e4d0b5eb2b36f229
AshanSandeep06 <ashansandeep06@gmail.com>
2023-02-13 10:46:28 +0530

    Created application.properties file for some configurations in JPAConfig Class

--- Back-End/src/main/java/lk/easy/carRental/config/JPAConfig.java
+++ Back-End/src/main/java/lk/easy/carRental/config/JPAConfig.java
@@ -2,6 +2,7 @@ package lk.easy.carRental.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -18,6 +19,7 @@ import javax.sql.DataSource;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories
+@PropertySource("classpath:application.properties")
 public class JPAConfig {
     @Autowired
     private Environment environment;
