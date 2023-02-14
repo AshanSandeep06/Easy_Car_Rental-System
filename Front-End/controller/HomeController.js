a6d6f92fc92a005cb74e13d572351052d142edcd
AshanSandeep06 <ashansandeep06@gmail.com>
2023-02-14 15:55:15 +0530

    Content Updated in frontend

---
+++ Front-End/controller/HomeController.js
@@ -0,0 +1,16 @@
+/*
+var swiper = new Swiper(".mySwiper", {
+    direction: "vertical",
+    autoplay: {
+        delay: 2600,
+        disableOnInteraction: false,
+    },
+    mousewheel: true,
+    keyboard: {
+        enabled: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});*/
